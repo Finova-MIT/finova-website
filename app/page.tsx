40f2cd5f2c03dda89a48032810a9cc122c5126ba
pranavgnn <pranavgnayak@gmail.com>
2025-01-16 01:13:16 +0530

    eslint bs

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,15 @@
 import { Separator } from "@/components/ui/separator"
 import Navbar from "@/components/ui/navbar"
 import Hero from "@/components/ui/hero"
-import About from "@/components/ui/about"
 import Domains from "@/components/ui/domains"
-import Projects from "@/components/ui/projects"
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Navbar />
       <Hero />
-      {/* <About /> */}
       <Domains />
       <Separator />
-      {/* <Projects /> */}
     </main>
   )
 }
