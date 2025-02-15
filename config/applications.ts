ef07fe81bd3fb33de7b559443b9d223d83ff1588
pranavgnn <pranavgnayak@gmail.com>
2025-02-15 18:54:48 +0530

    closed applications

--- config/applications.ts
+++ config/applications.ts
@@ -1,3 +1,4 @@
-export const isRecruitmentOpen = true;
+export const isRecruitmentOpen = false;
 export const redirectTime = 3;
-export const formLink = "https://docs.google.com/forms/d/e/1FAIpQLSelY1acKmIjjwwK5YDV1HWW2FvDB9JbB59O7srwTa19Gt55-Q/viewform"
+export const formLink =
+  "https://docs.google.com/forms/d/e/1FAIpQLSelY1acKmIjjwwK5YDV1HWW2FvDB9JbB59O7srwTa19Gt55-Q/viewform";
