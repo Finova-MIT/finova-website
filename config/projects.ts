8e64572977381ea3f25f4148b1a5f98ba93d8789
pranavgnn <pranavgnayak@gmail.com>
2025-02-15 20:24:43 +0530

    changed description of finova coin and ai chatbot

--- config/projects.ts
+++ config/projects.ts
@@ -3,23 +3,23 @@ export const completedProjects = [
     id: 1,
     url: "https://pump.fun/coin/D5MBXJKUTV2pBxdAJ43wrqu9gKZjURgjXjBkoFmhpump?coins_sort=market_cap",
     title: "FINOVA Coin",
-    description: "FINOVA Coin is the native digital asset of FINOVA, designed to revolutionize fintech innovation within our college ecosystem. It serves as a learning tool for blockchain, Web3, and crypto applications, enabling students to explore real-world use cases such as decentralized finance (DeFi), smart contracts, and tokenomics. FINOVA Coin fosters a hands-on approach to financial technology, empowering members to experiment with digital assets, build blockchain-based solutions, and contribute to the evolving fintech landscape.",
+    description: "Native digital asset of Finova",
     timestamp: "15-05-2024",
-    images: ["/projects/test1.jpg", "/projects/test2.jpg"],
+    images: ["/projects/finova-coin/demo1.png"],
   },
   {
     id: 2,
     url: "https://github.com/Finova-MIT/Trady-FinanceAI_LangGraph",
     title: "AI Chatbot",
-    description: "Intelligent conversational agent",
+    description: "Financial knowledge agent",
     timestamp: "22-06-2023",
-    images: ["/projects/test1.jpg", "/projects/test2.jpg"],
+    images: ["/projects/ai-chatbot/demo1.png"],
   },
 ];
 
 export const inProgressProjects = [
   {
-    id: 9,
+    id: 1,
     url: "",
     title: "Financial Data Scraper",
     description: "",
