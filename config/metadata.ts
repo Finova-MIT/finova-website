7d49c528deb7042306c99375393929b0c5431177
pranavgnn <pranavgnayak@gmail.com>
2025-02-16 13:52:42 +0530

    moved metadata to config

---
+++ config/metadata.ts
@@ -0,0 +1,8 @@
+export const metadata = {
+    websiteUrl: "https://www.finovamanipal.org",
+    shortName: "Finova",
+    title: "Finova Manipal | MIT's FinTech Club",
+    description: "Official website of Finova, Manipal Institute of Technology's premier FinTech club.",
+    keywords: "finova, fintech, mit, manipal, finance, technology, innovation, manipal institute of technology",
+    iconUrl: "/logo.png",
+}
