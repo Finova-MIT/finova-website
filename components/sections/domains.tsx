72c23f2383c805243fd3f8bf3711e857b2ca0fff
pranavgnn <pranavgnayak@gmail.com>
2025-02-18 00:27:40 +0530

    div optimization for mobile, desktop

--- components/sections/domains.tsx
+++ components/sections/domains.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { Button } from "../ui/button";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
@@ -9,82 +9,23 @@ import {
   leftDivAnimation,
   rightDivAnimation,
 } from "../../config/animations";
-// import { domains } from "@/config/domains";
+import { domains } from "@/config/domains";
 import Image from "next/image";
-
-import {
-  FaChartLine,
-  FaUniversity,
-  FaBitcoin,
-  FaShieldAlt,
-  FaDatabase,
-  FaCode,
-  FaBullhorn,
-  FaPalette,
-} from "react-icons/fa";
-
-const domains = [
-  {
-    name: "Quant and Algo Trading",
-    icon: FaChartLine,
-    description:
-      "Specialized in developing sophisticated algorithmic trading strategies and quantitative analysis tools for financial markets.",
-    image: "/domains/quant-and-algo-trading.jpg",
-  },
-  {
-    name: "Finance and Banking",
-    icon: FaUniversity,
-    description:
-      "Comprehensive solutions for banking systems, payment processing, and financial service applications.",
-    image: "/domains/finance-and-banking.jpg",
-  },
-  {
-    name: "Blockchain and Web3",
-    icon: FaBitcoin,
-    description:
-      "Expertise in blockchain technology, smart contracts, and decentralized application development.",
-    image: "/domains/blockchain-and-web3.jpg",
-  },
-  {
-    name: "Cybersecurity",
-    icon: FaShieldAlt,
-    description:
-      "Advanced security solutions and consulting services to protect digital assets and infrastructure.",
-    image: "/domains/cybersecurity.jpg",
-  },
-  {
-    name: "Data Analysis and Machine Learning",
-    icon: FaDatabase,
-    description:
-      "Leveraging AI and ML technologies for data-driven insights and predictive analytics.",
-    image: "/domains/data-analysis-and-ml.jpg",
-  },
-  {
-    name: "Fullstack Development",
-    icon: FaCode,
-    description:
-      "End-to-end web and application development using cutting-edge technologies and frameworks.",
-    image: "/domains/fullstack.jpg",
-  },
-  {
-    name: "Public Relations",
-    icon: FaBullhorn,
-    description:
-      "Strategic communication and brand management services for maximum market impact.",
-    image: "/domains/public-relations.jpg",
-  },
-  {
-    name: "Social Media and Graphic Design",
-    icon: FaPalette,
-    description:
-      "Creative digital content and visual design solutions for enhanced brand presence.",
-    image: "/domains/graphic-design.jpg",
-  },
-];
-
 
 export default function Domains() {
   const [selectedDomain, setSelectedDomain] = useState(domains[0]);
+  const [isMobile, setIsMobile] = useState<boolean>(true);
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkIfMobile();
+    window.addEventListener('resize', checkIfMobile);
+
+    return () => window.removeEventListener('resize', checkIfMobile);
+  }, []);
 
   return (
     <div
@@ -104,85 +45,89 @@ export default function Domains() {
       </h1>
 
       {/* Mobile View */}
-      {/* <motion.div
-        className="md:hidden space-y-2 max-w-md mx-auto"
-        initial="hidden"
-        whileInView="visible"
-        variants={bodyAnimation}
-        viewport={{ once: true }}
-      > */}
-      <div className="md:hidden space-y-2 max-w-md mx-auto">
-        {domains.map((domain, index) => (
-          <div key={index} className="rounded-lg p-4 border-border border-2">
-            <div className="flex items-center space-x-4">
-              <domain.icon className="text-3xl text-accent-foreground" />
-              <h3 className="font-medium">{domain.name}</h3>
+      {isMobile && (
+        /* <motion.div
+          className="md:hidden space-y-2 max-w-md mx-auto"
+          initial="hidden"
+          whileInView="visible"
+          variants={bodyAnimation}
+          viewport={{ once: true }}
+        > */
+        <div className="space-y-2 max-w-md mx-auto">
+          {domains.map((domain, index) => (
+            <div key={index} className="rounded-lg p-4 border-border border-2">
+              <div className="flex items-center space-x-4">
+                <domain.icon className="text-3xl text-accent-foreground" />
+                <h3 className="font-medium">{domain.name}</h3>
+              </div>
             </div>
-          </div>
-        ))}
-      {/* </motion.div> */}
-      </div>
+          ))}
+        {/* </motion.div> */}
+        </div>
+      )}
 
       {/* Desktop View */}
-      <div className="md:block lg:block hidden lg:-skew-x-6">
-        <div className="z-10 grid grid-cols-12 gap-12">
-          {/* Left Side - Domain List */}
-          {/* <motion.div
-            className="flex flex-col col-span-5 space-y-4"
-            initial="hidden"
-            whileInView="visible"
-            variants={leftDivAnimation}
-            viewport={{ once: true }}
-          > */}
-          <div className="flex flex-col col-span-5 space-y-4">
-            {domains.map((domain, index) => (
-              <Button
-                key={index}
-                className={`w-full cursor-pointer p-4 rounded-lg transition-transform duration-300 transform ${
-                  selectedDomain.name === domain.name
-                    ? "shadow-lg -translate-x-2 bg-gradient-to-r from-[#00c886] to-[#09a4c4] text-background"
-                    : "bg-background text-foreground hover:bg-muted"
-                }`}
-                onClick={() => setSelectedDomain(domain)}
-              >
-                <div className="flex items-center w-full space-x-2 transform lg:skew-x-6">
-                  <domain.icon className="text-3xl" />
-                  <h3 className="font-medium">{domain.name}</h3>
-                </div>
-              </Button>
-            ))}
-          {/* </motion.div> */}
-          </div>
-
-          {/* Right Side - Domain Details */}
-          {/* <motion.div
-            className="col-span-7 bg-muted/20 p-8"
-            initial="hidden"
-            whileInView="visible"
-            variants={rightDivAnimation}
-            viewport={{ once: true }}
-          > */}
-          <div className="col-span-7 bg-muted/20 p-8">
-            <div className="bg-background border-2 border-foreground/30 rounded-lg p-6 drop-shadow-[0px_0px_40px_rgba(0,255,253,0.2)] lg:skew-x-6">
-              <AspectRatio ratio={2 / 1} className="mb-4">
-                <Image
-                  width={1920}
-                  height={1080}
-                  src={selectedDomain.image}
-                  alt={selectedDomain.name}
-                  className="rounded-lg object-cover w-full h-full"
-                  loading="lazy"
-                />
-              </AspectRatio>
-              <h2 className="text-2xl font-bold mb-2">{selectedDomain.name}</h2>
-              <p className="text-muted-foreground leading-relaxed">
-                {selectedDomain.description}
-              </p>
+      {!isMobile && (
+        <div className="lg:-skew-x-6">
+          <div className="z-10 grid grid-cols-12 gap-12">
+            {/* Left Side - Domain List */}
+            {/* <motion.div
+              className="flex flex-col col-span-5 space-y-4"
+              initial="hidden"
+              whileInView="visible"
+              variants={leftDivAnimation}
+              viewport={{ once: true }}
+            > */}
+            <div className="flex flex-col col-span-5 space-y-4">
+              {domains.map((domain, index) => (
+                <Button
+                  key={index}
+                  className={`w-full cursor-pointer p-4 rounded-lg transition-transform duration-300 transform ${
+                    selectedDomain.name === domain.name
+                      ? "shadow-lg -translate-x-2 bg-gradient-to-r from-[#00c886] to-[#09a4c4] text-background"
+                      : "bg-background text-foreground hover:bg-muted"
+                  }`}
+                  onClick={() => setSelectedDomain(domain)}
+                >
+                  <div className="flex items-center w-full space-x-2 transform lg:skew-x-6">
+                    <domain.icon className="text-3xl" />
+                    <h3 className="font-medium">{domain.name}</h3>
+                  </div>
+                </Button>
+              ))}
+            {/* </motion.div> */}
             </div>
-          {/* </motion.div> */}
+
+            {/* Right Side - Domain Details */}
+            {/* <motion.div
+              className="col-span-7 bg-muted/20 p-8"
+              initial="hidden"
+              whileInView="visible"
+              variants={rightDivAnimation}
+              viewport={{ once: true }}
+            > */}
+            <div className="col-span-7 bg-muted/20 p-8">
+              <div className="bg-background border-2 border-foreground/30 rounded-lg p-6 drop-shadow-[0px_0px_40px_rgba(0,255,253,0.2)] lg:skew-x-6">
+                <AspectRatio ratio={2 / 1} className="mb-4">
+                  <Image
+                    width={1920}
+                    height={1080}
+                    src={selectedDomain.image}
+                    alt={selectedDomain.name}
+                    className="rounded-lg object-cover w-full h-full"
+                    loading="lazy"
+                  />
+                </AspectRatio>
+                <h2 className="text-2xl font-bold mb-2">{selectedDomain.name}</h2>
+                <p className="text-muted-foreground leading-relaxed">
+                  {selectedDomain.description}
+                </p>
+              </div>
+            {/* </motion.div> */}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
 import {
@@ -105,6 +105,18 @@ export default function Projects() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [mobileSelectedProject, setMobileSelectedProject] =
     useState<Project | null>(null);
+  const [isMobile, setIsMobile] = useState(true);
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    checkIfMobile();
+    window.addEventListener("resize", checkIfMobile);
+
+    return () => window.removeEventListener("resize", checkIfMobile);
+  }, []);
 
   const handleMobileLearnMore = (project: Project) => {
     setMobileSelectedProject(project);
@@ -123,178 +135,182 @@ export default function Projects() {
         variants={headingAnimation}
         viewport={{ once: true }}
       > */}
-
       <h1 className="text-4xl font-bold text-center md:py-12 lg:py-16 py-6">
         Our Projects
-      {/* </motion.h1> */}
+        {/* </motion.h1> */}
       </h1>
 
       {/* Mobile View */}
-      {/* <motion.div
-        className="md:hidden space-y-2 max-w-md mx-auto"
-        initial="hidden"
-        whileInView="visible"
-        variants={bodyAnimation}
-        viewport={{ once: true }}
-      > */}
-
-      <div className="md:hidden space-y-2 max-w-md mx-auto">
-        <Tabs defaultValue="completed" className="w-full">
-          <TabsList className="flex space-x-4 justify-center mb-4">
-            <TabsTrigger value="completed">Completed</TabsTrigger>
-            <TabsTrigger value="in-progress">In Progress</TabsTrigger>
-          </TabsList>
-          {["completed", "in-progress"].map((tab) => (
-            <TabsContent key={tab} value={tab}>
-              <div className="space-y-4">
-                {(tab === "completed"
-                  ? completedProjects
-                  : inProgressProjects
-                ).map((project) => (
-                  <div
-                    key={project.id}
-                    className="border-border border-2 rounded-lg p-4 grid"
-                  >
-                    <h3 className="font-bold text-lg mb-2">{project.title}</h3>
-                    <p className="text-muted-foreground mb-4">
-                      {project.description}
-                    </p>
-                    <Button
-                      className=""
-                      onClick={() => handleMobileLearnMore(project)}
-                    >
-                      Learn More
-                    </Button>
-                  </div>
-                ))}
-              </div>
-            </TabsContent>
-          ))}
-        </Tabs>
-
-        {/* Mobile Project Details Modal */}
-        {mobileSelectedProject && (
-          <ProjectDetailsModal
-            project={mobileSelectedProject}
-            isOpen={isModalOpen}
-            onClose={() => setIsModalOpen(false)}
-          />
-        )}
-      {/* </motion.div> */}
-      </div>
-
-      {/* Desktop View */}
-      <div className="md:block lg:block hidden lg:-skew-x-6">
-        <div className="z-10 grid grid-cols-12 gap-12">
-          {/* Left Side - Project Details */}
-          {/* <motion.div
-            className="col-span-7 bg-muted/20 p-8"
-            initial="hidden"
-            whileInView="visible"
-            variants={leftDivAnimation}
-            viewport={{ once: true }}
-          > */}
-          <div className="col-span-7 bg-muted/20 p-8">
-            <div className="bg-background border-2 border-foreground/30 rounded-lg p-6 drop-shadow-[0px_0px_40px_rgba(0,255,253,0.2)] lg:skew-x-6">
-              <Carousel className="mb-4">
-                <CarouselContent>
-                  {selectedProject.images.map((image, index) => (
-                    <CarouselItem key={index}>
-                      <div className="aspect-[2/1]">
-                        <Image
-                          width={1920}
-                          height={1080}
-                          src={image}
-                          alt={`${selectedProject.title} - Image ${index + 1}`}
-                          className="rounded-lg object-cover w-full h-full"
-                          loading="lazy"
-                        />
-                      </div>
-                    </CarouselItem>
-                  ))}
-                </CarouselContent>
-                <CarouselPrevious />
-                <CarouselNext />
-              </Carousel>
-              <h2 className="text-2xl font-bold mb-2 hover:text-chart-1 transition-colors duration-300">
-                <Link
-                  href={selectedProject.url}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {selectedProject.title}
-                </Link>
-              </h2>
-              <p className="text-sm text-muted-foreground mb-4 flex gap-2 items-center">
-                <FaCalendar className="text-md" />
-                {selectedProject.timestamp}
-              </p>
-              <p className="text-muted-foreground leading-relaxed">
-                {selectedProject.description}
-              </p>
-            </div>
-          {/* </motion.div> */}
-          </div>
-
-          {/* Right Side - Project List */}
-          {/* <motion.div
-            className="flex flex-col col-span-5"
-            initial="hidden"
-            whileInView="visible"
-            variants={rightDivAnimation}
-            viewport={{ once: true }}
-          > */}
-          <div className="flex flex-col col-span-5">
-            <Tabs
-              defaultValue="completed"
-              className="w-full"
-              onValueChange={(value) => {
-                setActiveTab(value as "completed" | "in-progress");
-                setSelectedProject(
-                  value === "completed"
-                    ? completedProjects[0]
-                    : inProgressProjects[0]
-                );
-              }}
-            >
-              <TabsList className="flex justify-start space-x-4 mb-4">
-                <TabsTrigger value="completed">
-                  <span className="lg:skew-x-6">Completed</span>
-                </TabsTrigger>
-                <TabsTrigger value="in-progress">
-                  <span className="lg:skew-x-6">In Progress</span>
-                </TabsTrigger>
-              </TabsList>
-
-              {["completed", "in-progress"].map((tab) => (
-                <TabsContent key={tab} value={tab} className="mt-0 space-y-4">
+      {isMobile && (
+        /* <motion.div
+          className="md:hidden space-y-2 max-w-md mx-auto"
+          initial="hidden"
+          whileInView="visible"
+          variants={bodyAnimation}
+          viewport={{ once: true }}
+        > */
+        <div className="space-y-2 max-w-md mx-auto">
+          <Tabs defaultValue="completed" className="w-full">
+            <TabsList className="flex space-x-4 justify-center mb-4">
+              <TabsTrigger value="completed">Completed</TabsTrigger>
+              <TabsTrigger value="in-progress">In Progress</TabsTrigger>
+            </TabsList>
+            {["completed", "in-progress"].map((tab) => (
+              <TabsContent key={tab} value={tab}>
+                <div className="space-y-4">
                   {(tab === "completed"
                     ? completedProjects
                     : inProgressProjects
                   ).map((project) => (
-                    <Button
+                    <div
                       key={project.id}
-                      className={`w-full cursor-pointer p-4 rounded-lg transition-transform duration-300 transform ${
-                        selectedProject.id === project.id
-                          ? "shadow-lg -translate-x-2 bg-gradient-to-r from-[#00c886] to-[#09a4c4] text-background"
-                          : "bg-background text-foreground hover:bg-muted"
-                      }`}
-                      onClick={() => setSelectedProject(project)}
+                      className="border-border border-2 rounded-lg p-4 grid"
                     >
-                      <div className="flex items-center space-x-4 w-full justify-start transform lg:skew-x-6">
-                        <h3 className="font-medium text-left">
-                          {project.title}
-                        </h3>
-                      </div>
-                    </Button>
+                      <h3 className="font-bold text-lg mb-2">
+                        {project.title}
+                      </h3>
+                      <p className="text-muted-foreground mb-4">
+                        {project.description}
+                      </p>
+                      <Button
+                        className=""
+                        onClick={() => handleMobileLearnMore(project)}
+                      >
+                        Learn More
+                      </Button>
+                    </div>
                   ))}
-                </TabsContent>
-              ))}
-            </Tabs>
+                </div>
+              </TabsContent>
+            ))}
+          </Tabs>
+          {/* Mobile Project Details Modal */}
+          {mobileSelectedProject && (
+            <ProjectDetailsModal
+              project={mobileSelectedProject}
+              isOpen={isModalOpen}
+              onClose={() => setIsModalOpen(false)}
+            />
+          )}
           {/* </motion.div> */}
+        </div>
+      )}
+
+      {/* Desktop View */}
+      {!isMobile && (
+        <div className="lg:-skew-x-6">
+          <div className="z-10 grid grid-cols-12 gap-12">
+            {/* Left Side - Project Details */}
+            {/* <motion.div
+              className="col-span-7 bg-muted/20 p-8"
+              initial="hidden"
+              whileInView="visible"
+              variants={leftDivAnimation}
+              viewport={{ once: true }}
+            > */}
+            <div className="col-span-7 bg-muted/20 p-8">
+              <div className="bg-background border-2 border-foreground/30 rounded-lg p-6 drop-shadow-[0px_0px_40px_rgba(0,255,253,0.2)] lg:skew-x-6">
+                <Carousel className="mb-4">
+                  <CarouselContent>
+                    {selectedProject.images.map((image, index) => (
+                      <CarouselItem key={index}>
+                        <div className="aspect-[2/1]">
+                          <Image
+                            width={1920}
+                            height={1080}
+                            src={image}
+                            alt={`${selectedProject.title} - Image ${
+                              index + 1
+                            }`}
+                            className="rounded-lg object-cover w-full h-full"
+                            loading="lazy"
+                          />
+                        </div>
+                      </CarouselItem>
+                    ))}
+                  </CarouselContent>
+                  <CarouselPrevious />
+                  <CarouselNext />
+                </Carousel>
+                <h2 className="text-2xl font-bold mb-2 hover:text-chart-1 transition-colors duration-300">
+                  <Link
+                    href={selectedProject.url}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {selectedProject.title}
+                  </Link>
+                </h2>
+                <p className="text-sm text-muted-foreground mb-4 flex gap-2 items-center">
+                  <FaCalendar className="text-md" />
+                  {selectedProject.timestamp}
+                </p>
+                <p className="text-muted-foreground leading-relaxed">
+                  {selectedProject.description}
+                </p>
+              </div>
+              {/* </motion.div> */}
+            </div>
+
+            {/* Right Side - Project List */}
+            {/* <motion.div
+              className="flex flex-col col-span-5"
+              initial="hidden"
+              whileInView="visible"
+              variants={rightDivAnimation}
+              viewport={{ once: true }}
+            > */}
+            <div className="flex flex-col col-span-5">
+              <Tabs
+                defaultValue="completed"
+                className="w-full"
+                onValueChange={(value) => {
+                  setActiveTab(value as "completed" | "in-progress");
+                  setSelectedProject(
+                    value === "completed"
+                      ? completedProjects[0]
+                      : inProgressProjects[0]
+                  );
+                }}
+              >
+                <TabsList className="flex justify-start space-x-4 mb-4">
+                  <TabsTrigger value="completed">
+                    <span className="lg:skew-x-6">Completed</span>
+                  </TabsTrigger>
+                  <TabsTrigger value="in-progress">
+                    <span className="lg:skew-x-6">In Progress</span>
+                  </TabsTrigger>
+                </TabsList>
+                {["completed", "in-progress"].map((tab) => (
+                  <TabsContent key={tab} value={tab} className="mt-0 space-y-4">
+                    {(tab === "completed"
+                      ? completedProjects
+                      : inProgressProjects
+                    ).map((project) => (
+                      <Button
+                        key={project.id}
+                        className={`w-full cursor-pointer p-4 rounded-lg transition-transform duration-300 transform ${
+                          selectedProject.id === project.id
+                            ? "shadow-lg -translate-x-2 bg-gradient-to-r from-[#00c886] to-[#09a4c4] text-background"
+                            : "bg-background text-foreground hover:bg-muted"
+                        }`}
+                        onClick={() => setSelectedProject(project)}
+                      >
+                        <div className="flex items-center space-x-4 w-full justify-start transform lg:skew-x-6">
+                          <h3 className="font-medium text-left">
+                            {project.title}
+                          </h3>
+                        </div>
+                      </Button>
+                    ))}
+                  </TabsContent>
+                ))}
+              </Tabs>
+              {/* </motion.div> */}
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
