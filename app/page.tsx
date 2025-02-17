42ea1c55e92e4d6a37c6f382a667904f96449474
pranavgnn <pranavgnayak@gmail.com>
2025-02-17 12:25:33 +0530

    added generateMetadata()

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+import type { Metadata } from "next";
+import { metadata as metadataConfig } from "@/config/metadata";
+
 import Hero from "@/components/sections/hero";
 import About from "@/components/sections/about";
 import Domains from "@/components/sections/domains";
 import Projects from "@/components/sections/projects";
 import Contact from "@/components/sections/contact";
+
+export const generateMetadata = (): Metadata => {
+  return {
+    title: `Home | ${metadataConfig.shortName}`,
+    description: "Homepage of Finova Manipal - A student-run FinTech club at Manipal Institute of Technology",
+  };
+};
 
 export default function Home() {
   return (
