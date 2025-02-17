8408ea53e6399e0b2ed950667cdb0226c2b081b0
pranavgnn <pranavgnayak@gmail.com>
2025-02-17 12:23:33 +0530

    changed redirect time from 3 to 1

--- config/applications.ts
+++ config/applications.ts
@@ -1,4 +1,4 @@
 export const isRecruitmentOpen = false;
-export const redirectTime = 3;
+export const redirectTime = 1;
 export const formLink =
   "https://docs.google.com/forms/d/e/1FAIpQLSelY1acKmIjjwwK5YDV1HWW2FvDB9JbB59O7srwTa19Gt55-Q/viewform";
