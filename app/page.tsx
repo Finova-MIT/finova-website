978b9b66d19027f814aef00daed0c0b71d0baae6
pranavgnn <pranavgnayak@gmail.com>
2025-02-19 06:40:46 +0530

    dynamic loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import type { Metadata } from "next";
+import dynamic from "next/dynamic";
 import { metadata as metadataConfig } from "@/config/metadata";
 
-import Hero from "@/components/sections/hero";
-import About from "@/components/sections/about";
-import Domains from "@/components/sections/domains";
-import Projects from "@/components/sections/projects";
-import Contact from "@/components/sections/contact";
+const Hero = dynamic(() => import("@/components/sections/hero"));
+const About = dynamic(() => import("@/components/sections/about"));
+const Domains = dynamic(() => import("@/components/sections/domains"));
+const Projects = dynamic(() => import("@/components/sections/projects"));
+const Contact = dynamic(() => import("@/components/sections/contact"));
+
+// import Hero from "@/components/sections/hero";
+// import About from "@/components/sections/about";
+// import Domains from "@/components/sections/domains";
+// import Projects from "@/components/sections/projects";
+// import Contact from "@/components/sections/contact";
 
 export const generateMetadata = (): Metadata => {
   return {
